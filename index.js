780374e3d4a96947924858192722bcde3c1ce5c1
Vieillepatacrepe <46195750+Vieillepatacrepe@users.noreply.github.com>
2019-01-13 10:32:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const bot = new Discord.Client();
 
 var prefix = ("/")
 
+bot.on("message", message => {
+  if (message.content === "a"){
+    message.reply("b")
+    
 bot.login(process.env.Token);
