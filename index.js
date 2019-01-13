135f60d1cee6e9f5eea68dd24f016304418f0a2c
Vieillepatacrepe <46195750+Vieillepatacrepe@users.noreply.github.com>
2019-01-13 14:51:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-bot.login(process.env.Token);
+
+var prefix = ("/")
+
+bot.on ("ready",function() {
+    bot.user.setGame("Commande: helpmeimdying");
+    console.log("Connected");
+});
+
+bot.on('message',message => {
+    if (message.content === prefix + "helpmeindying") {
+        message.channel.sendMessage("vas prendre un défibrilateur");
+    }
+
+    if (message.content === "salut mon gros");
+    message.reply("bonsoir");
+    console.log("commande bonjour ok");
+}
+
+  });
+  bot.login(process.env.TOKEN);
